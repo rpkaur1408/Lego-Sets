eff3069095d0fb68c8a4c22f5c1f80813093672b
Rehatpreet kaur <rehatpreetkaur981@gmail.com>
2024-08-22 22:19:16 -0400

    Expressing in server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080; 
+
 const legoData = require("./modules/legoSets");
+
+legoData.initialize().then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch(err => {
+    console.error("Failed to initialize Lego data:", err);
+})
